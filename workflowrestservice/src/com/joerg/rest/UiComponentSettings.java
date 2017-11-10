0aa04a037f92bbd2b30643d9cac2eba70ad090e3
Joey73 <schmitz873@gmail.com>
2017-11-10 16:03:44 +0100

    Several improvements

--- workflowrestservice/src/com/joerg/rest/UiComponentSettings.java
+++ workflowrestservice/src/com/joerg/rest/UiComponentSettings.java
@@ -1,9 +1,13 @@
 package com.joerg.rest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.joerg.db.WorkflowDbDummyData;
 import com.joerg.rest.dtos.UiComponentSettingsDto;
@@ -16,7 +20,7 @@ public class UiComponentSettings {
 	@Path("/getall")
 	@Produces("application/json")
 	public UiComponentSettingsListDto getAllUiComponentSettings(){
-		//http://localhost:8080/workflowrestservice/rest/uicomponentsettings/getall
+		//http://localhost:18080/workflowrestservice/rest/uicomponentsettings/getall
 		
 		WorkflowDbDummyData workflowDbDummyData = new WorkflowDbDummyData();
 		UiComponentSettingsListDto uiComponentSettingsListDto = workflowDbDummyData.getAllUiComponentSettings();
@@ -31,16 +35,33 @@ public class UiComponentSettings {
 	@Path("/get/{componentId}")
 	@Produces("application/json")
 	public UiComponentSettingsDto getComponentSettings(@PathParam("componentId") String componentId){
-		//http://localhost:8080/workflowrestservice/rest/uicomponentsettings/get/{componentId}
+		//http://localhost:18080/workflowrestservice/rest/uicomponentsettings/get/{componentId}
 		
 		System.out.println("componentId: " + componentId);
 
 		WorkflowDbDummyData workflowDbDummyData = new WorkflowDbDummyData();
-		UiComponentSettingsDto uiComponentSettings = workflowDbDummyData.getUiComponentSettings(componentId);
-		System.out.println("uiComponentSettings.isEnabled: " + uiComponentSettings.isEnabled());
-		return uiComponentSettings;
+		UiComponentSettingsDto uiComponentSettingsDto = workflowDbDummyData.getUiComponentSettings(componentId);
+		System.out.println("uiComponentSettings.isEnabled: " + uiComponentSettingsDto.isEnabled());
+		return uiComponentSettingsDto;
 
 //		WorkflowDb workflowDb = new WorkflowDb();
 //		return workflowDb.getProcessData(processInstanceId);
+	}
+	
+	@POST
+    @Path("/update")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response updateUiComponentSetting(UiComponentSettingsDto uiComponentSettingsDto) {
+		//http://localhost:18080/workflowrestservice/rest/uicomponentsettings/update
+		if(uiComponentSettingsDto == null) {
+			return Response.status(204).build();
+		}
+		System.out.println("uiComponentSettingsDto.getComponentId(): " + uiComponentSettingsDto.getComponentId());
+		System.out.println("uiComponentSettingsDto.isVisible(): " + uiComponentSettingsDto.isVisible());
+		System.out.println("uiComponentSettingsDto.isEnabled(): " + uiComponentSettingsDto.isEnabled());
+		System.out.println("uiComponentSettingsDto.isRequired(): " + uiComponentSettingsDto.isRequired());
+		String output = "POST: Jersey says: " + uiComponentSettingsDto.getComponentId();
+		return Response.status(200).entity(uiComponentSettingsDto).build();
 	}
 }
